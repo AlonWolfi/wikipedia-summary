2f1d97c69c278c895b863731c4b97ef5fb8ef372
AlonWolfi <alonwolfy@gmail.com>
2020-02-22 16:07:06 +0200

    added text for utils

---
+++ tests/utils/test_utils.py
@@ -0,0 +1,16 @@
+from unittest import TestCase
+import pathlib
+
+from utils.utils import *
+
+class UtilsTest(TestCase):
+    def test_task_done(self):
+        test_name = 'test'
+        task_done(test_name)
+        from utils.json_utils import get_config
+        path = pathlib.Path.cwd().parents[1] / get_config()['cache_dir'] / (test_name + '.done')
+        self.assertTrue(os.path.exists(path))
+        self.assertTrue(os.path.isfile(path))
+        os.remove(path)
+    def test_get_project_dir(self):
+        self.assertEqual(get_project_dir(), pathlib.Path.cwd().parents[1])
